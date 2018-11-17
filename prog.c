cfe77fa6aedc44ff8f006bb01361ff22ba6db0ea
Иван Оглоблин <semen_ivan@bk.ru>
2018-11-17 11:46:00 +0700

    Update prog.c

--- prog.c
+++ prog.c
@@ -1,25 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+void DebugArr(int * arr, int n){
+  for (int i = 0; i < n; i++)
+    printf("%d ",  arr[i]);
+  printf("\n");
+}
+
+int* merge(int *elementA, int *elementB, int lengthA, int lengthB) {
+  int* too = malloc(sizeof(int) * (lengthA + lengthB));
+  int a = 0;
+  int b = 0;
+  for (int i = 0; i < lengthA + lengthB;) {
+    if (a < lengthA && elementA[a] >= elementB[b])
+      too[i++] = elementA[a++];
+
+    if (b >= lengthB)
+      too[i++] = elementA[a++];
+      
+    if (a >= lengthA)
+      too[i++] = elementB[b++];
+  
+    if (b < lengthB && elementB[b] >= elementA[a])
+      too[i++] = elementB[b++];
+  }
+  return too;
+}
+
+int* sort(int * mass, int length){
+  if(length == 1){
+    return mass;
+  }
+  int midle = length/2;
+  return merge(&(sort(&mass[0], length/2)[0]), &(sort(&mass[length/2], length - length/2)[0]), length/2, length - length/2);
+}
 
 int main(void) {
-  int N;
-  scanf("%d", &N);
-  int *Mass = (int*)malloc(sizeof(int) * N);
-  for (int i = 0; i < N; i++)
-  {
-    scanf("%d", &Mass[i]);
-  }
-  for (int i = 0; i < N-1; i++)
-    for (int p = 0; p < N-1; p++)
-      if (Mass[p] > Mass[p + 1]) {
-        int some = Mass[p];
-        Mass[p] = Mass[p + 1];
-        Mass[p + 1] = some;
-      }
-  for (int i = N - 1; i >= 0; i--)
-    printf("%d ", Mass[i]);
-  
-  printf("\n");
-  free(Mass);
+  int n;
+  scanf("%d", &n);
+  int *arr = (int*)malloc(sizeof(int) * n);
+  for (int i = 0; i < n; i++)
+    scanf("%d", &arr[i]);
+  int *Mass = sort(&arr[0], n);
+  DebugArr(Mass, n);
   return 0;
 }
